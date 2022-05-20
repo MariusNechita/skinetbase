6e70b8ec8940fc12de6fead534a5de837b1871f7
Marius Nechita <marius.nechita.ext@siemens.com>
2022-05-20 11:15:16 +0300

    add breadcrumb service && improve UI

--- client/src/app/core/interceptors/error.interceptor.ts
+++ client/src/app/core/interceptors/error.interceptor.ts
@@ -7,7 +7,7 @@ import {
 } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { NavigationExtras, Router } from '@angular/router';
-import { catchError } from 'rxjs/operators';
+import { catchError, delay } from 'rxjs/operators';
 import { ToastrService } from 'ngx-toastr';
 
 
@@ -24,7 +24,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             if (error.error.errors) {
               throw error.error;
             } else {
-            this.toastr.error(error.error.message, error.error.statusCode);
+              this.toastr.error(error.error.message, error.error.statusCode);
             }
           }
           if (error.status === 401) {
@@ -34,7 +34,7 @@ export class ErrorInterceptor implements HttpInterceptor {
             this.router.navigateByUrl('/not-found');
           }
           if (error.status === 500) {
-            const navigationExtras: NavigationExtras = {state: {error: error.error}};
+            const navigationExtras: NavigationExtras = { state: { error: error.error } };
             this.router.navigateByUrl('/server-error', navigationExtras)
           }
 
